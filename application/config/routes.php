753008de0b67d47825f4c4773e6403a3a8989a54
Atik-Tambakwale <atik20202019@gmail.com>
2021-05-31 12:13:06 +0530

    changes in routes file

--- application/config/routes.php
+++ application/config/routes.php
@@ -4,6 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 //DEFAULT ROUTES
 $route['default_controller'] = 'C_Tour_Process';
 $route['404_override'] = 'My404';
-$route['translate_uri_dashes'] = FALSE;
+$route['translate_uri_dashes'] = TRUE;
 
 $route['tutotial/get_videos']='C_Tour_Process/get_totutial_videos';
