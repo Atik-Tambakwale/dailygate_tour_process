1761a56ae5b6f0492ed30a50aaecdb7dcf81cc74
Atik-Tambakwale <atik20202019@gmail.com>
2021-05-31 11:49:01 +0530

    change 404 error route

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 //DEFAULT ROUTES
 $route['default_controller'] = 'C_Tour_Process';
-$route['404_override'] = '';
+$route['404_override'] = 'My404';
 $route['translate_uri_dashes'] = FALSE;
 
 $route['tutotial/get_videos']='C_Tour_Process/get_totutial_videos';
